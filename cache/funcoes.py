31a6b84eeb26e9ab76790a541e5bf65260e09020
tigabriel09-coder <ti.gabriel09@gmail.com>
2026-05-01 12:52:07 -0300

    guardar dado

--- cache/funcoes.py
+++ cache/funcoes.py
@@ -10,4 +10,12 @@ def rolar_dados(qtd):
     
     return resultados
 
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar]
+    
+    dados_no_estoque.append(dado)
+    
+    dados_rolados.pop(dado_para_guardar)
+    
+    return [dados_rolados, dados_no_estoque]
     
