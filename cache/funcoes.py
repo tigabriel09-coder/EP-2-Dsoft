9ec6815a4fb29a55729178e5e102c05c3b8eb45c
tigabriel09-coder <ti.gabriel09@gmail.com>
2026-05-01 15:01:46 -0300

    regra avancada

--- cache/funcoes.py
+++ cache/funcoes.py
@@ -127,7 +127,7 @@ def calcula_pontos_quina(dados):
     
     return 0
 
-def calcula_pontos_regra_avancada(dados):
+def calcula_pontos_regra_avancada    (dados):
     return {
         'cinco_iguais': calcula_pontos_quina(dados),
         'full_house': calcula_pontos_full_house(dados),
@@ -136,6 +136,4 @@ def calcula_pontos_regra_avancada(dados):
         'sequencia_alta': calcula_pontos_sequencia_alta(dados),
         'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)
     }
-#sem necessidade de definir e depois retornar, direto no return funciona
-
-print(calcula_pontos_regra_avancada([4, 4, 4, 4, 4]))
+#sem necessidade de definir e depois retornar, direto no return funcion
