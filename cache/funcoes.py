547979cf6364afde7db8431e9becea6918980540
tigabriel09-coder <ti.gabriel09@gmail.com>
2026-05-01 14:20:52 -0300

    regra avançada

--- cache/funcoes.py
+++ cache/funcoes.py
@@ -126,3 +126,12 @@ def calcula_pontos_quina(dados):
             return 50
     
     return 0
+
+def calcula_pontos_regra_avancada(dados):
+    return {
+        'cinco_iguais': calcula_pontos_quina(dados),
+        'full_house': calcula_pontos_full_house(dados),
+        'quadra': calcula_pontos_quadra(dados),
+        'sem_combinacao': calcula_pontos_soma(dados),
+        'sequencia_alta': calcula_pontos_sequencia_alta(dados),
+        'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)}
