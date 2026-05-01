050a3cbc8f3fb8ee9f0ed24e2ab053b04f1f1561
tigabriel09-coder <ti.gabriel09@gmail.com>
2026-05-01 15:24:00 -0300

    final

--- cache/programa.py
+++ cache/programa.py
@@ -1,7 +1,8 @@
 import random
+from cache.funcoes import faz_jogada, guardar_dado, imprime_cartela, remover_dado
 from funcoes import *
 
-# inicializa cartela
+
 cartela = {
     'regra_simples': {i: -1 for i in range(1, 7)},
     'regra_avancada': {
@@ -36,15 +37,15 @@ for rodada in range(12):
         if opcao == "1":
             print("Digite o índice do dado a ser guardado (0 a 4):")
             idx = int(input(">"))
-            dados_rolados, dados_guardados = funcoes.guardar_dado(dados_rolados, dados_guardados, idx)
+            dados_rolados, dados_guardados = guardar_dado(dados_rolados, dados_guardados, idx)
 
-        # ---------------- REMOVER ----------------
+        
         elif opcao == "2":
             print("Digite o índice do dado a ser removido (0 a 4):")
             idx = int(input(">"))
             dados_rolados, dados_guardados = remover_dado(dados_rolados, dados_guardados, idx)
 
-        # ---------------- RERROLAR ----------------
+        
         elif opcao == "3":
             if rerrolagens >= 2:
                 print("Você já usou todas as rerrolagens.")
@@ -52,17 +53,17 @@ for rodada in range(12):
                 dados_rolados = rolar_dados(5 - len(dados_guardados))
                 rerrolagens += 1
 
-        # ---------------- VER CARTELA ----------------
+        
         elif opcao == "4":
             imprime_cartela(cartela)
 
-        # ---------------- FAZER JOGADA ----------------
+        
         elif opcao == "0":
             while True:
                 print("Digite a combinação desejada:")
                 categoria = input(">")
 
-                # verifica validade
+                
                 if categoria in ["1","2","3","4","5","6"]:
                     cat = int(categoria)
                     if cartela['regra_simples'][cat] != -1:
@@ -81,33 +82,28 @@ for rodada in range(12):
                 else:
                     print("Combinação inválida. Tente novamente.")
 
-            break  # termina a rodada
+            break  
 
-        # ---------------- OPÇÃO INVÁLIDA ----------------
+       
         else:
             print("Opção inválida. Tente novamente.")
 
 
-# ---------------- FINAL DO JOGO ----------------
 
 imprime_cartela(cartela)
 
-# soma total
 total = 0
 
-# soma simples
 soma_simples = 0
 for i in cartela['regra_simples']:
     if cartela['regra_simples'][i] != -1:
         soma_simples += cartela['regra_simples'][i]
         total += cartela['regra_simples'][i]
 
-# soma avançada
 for i in cartela['regra_avancada']:
     if cartela['regra_avancada'][i] != -1:
         total += cartela['regra_avancada'][i]
 
-# bônus
 if soma_simples >= 63:
     total += 35
 
